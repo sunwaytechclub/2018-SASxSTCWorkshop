881c70112ab28b557be6ffa7d9712f9be8e53d62
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2018-10-16 12:45:01 +0800

    Implement scrapping factbook

--- sasxstc/sasxstc/settings.py
+++ sasxstc/sasxstc/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'sasxstc.spiders'
 #USER_AGENT = 'sasxstc (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -39,32 +39,32 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'sasxstc.middlewares.SasxstcSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'sasxstc.middlewares.SasxstcDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'sasxstc.pipelines.SasxstcPipeline': 300,
 #}
 
---
+++ sasxstc/sasxstc/spiders/FactbookSpider.py
@@ -0,0 +1,61 @@
+from pprint import pprint
+import scrapy
+
+
+class FactbookSpider(scrapy.Spider):
+    name = "factbook"
+    start_urls = [
+        'https://www.cia.gov/library/publications/the-world-factbook/rankorder/rankorderguide.html'
+    ]
+
+    '''
+    Called after every request
+    This is where your scrapping code should be
+    '''
+
+    def parse(self, response):
+        links = response.xpath('//body//div[@id="profileguide"]/div[@class="answer"]//a')
+        results = {}
+        for index, link in enumerate(links):
+            results[link.xpath('text()').extract_first()] = link.xpath('@href').extract_first()
+
+        yield scrapy.Request(
+            response.urljoin(results["Population growth rate:"]),
+            callback=self.parse_population,
+            meta={"initial_response": response, "links": results}
+        )
+
+    def parse_population(self, response):
+        meta = response.meta
+        rows = response.xpath('//div[@class="wfb-text-box"]//table[@id="rankOrder"]/tbody/tr')
+        results = {}
+        for index, row in enumerate(rows):
+            if not row.xpath('@class').extract_first() == "rankHeading":
+                results[row.xpath('@id').extract_first()] = {
+                    "name": row.xpath('td[@class="region"]//text()').extract_first(),
+                    "population_growth_rate": row.xpath('td[3]/text()').extract_first()
+                }
+        meta["results"] = results
+        yield scrapy.Request(
+            meta["initial_response"].urljoin(meta["links"]["GDP - real growth rate:"]),
+            callback=self.parse_gdp,
+            meta=meta
+        )
+
+    def parse_gdp(self, response):
+        meta = response.meta
+        results = meta["results"]
+
+        rows = response.xpath('//div[@class="wfb-text-box"]//table[@id="rankOrder"]/tbody/tr')
+        for index, row in enumerate(rows):
+            if not row.xpath('@class').extract_first() == 'rankHeading':
+                id = row.xpath('@id').extract_first()
+                try:
+                    results[id]["gdp_growth_rate"] = row.xpath('td[3]/text()').extract_first()
+                except KeyError:
+                    results[id] = {
+                        "name": row.xpath('td[@class="region"]//text()').extract_first(),
+                        "gdp_growth_rate": row.xpath('td[3]/text()').extract_first()
+                    }
+
+        return results
